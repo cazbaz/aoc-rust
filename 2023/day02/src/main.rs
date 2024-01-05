3608863a1096787b036bb80e710a8381dc60ab28
Steam Deck User <zbdevx@gmail.com>
2024-01-04 20:33:30 -0700

    Starting on day 2

---
+++ 2023/day02/src/main.rs
@@ -0,0 +1,70 @@
+
+/*
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
+
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+*/
+
+use std::fs;
+
+fn main() {
+    println!("{}", parse_games())
+}
+
+struct Game {
+    id: u32,
+    red: u16,
+    green: u16,
+    blue: u16
+}
+
+fn parse_games() -> u32 {
+    let mut result: u32 = 0;
+
+    let input = fs::read_to_string("input").expect("Unable to read input file.");
+
+    for line in input.lines() {
+        let game: Game = parse_into_game(line.to_string());
+
+        result += game.id
+    }
+
+    return result; 
+}
+
+fn parse_into_game(line: String) -> Game {
+    let name_result_split: Vec<&str> = line.split(":").collect();
+    let game_name: Vec<&str> = name_result_split[0].split(" ").collect(); 
+    let game_id: String = String::from(game_name[1]);
+    let all_results: Vec<&str> = name_result_split[1].split(";").collect();
+    
+    let mut red_result: &str = "";
+    let mut green_result: &str = "";
+    let mut blue_result: &str = "";
+
+    for result in all_results {
+        if result.contains("red") {
+            red_result = result.split(" ").collect::<Vec<&str>>()[0];
+        }
+
+        if result.contains("green") {
+            green_result = result.split(" ").collect::<Vec<&str>>()[0];
+        }
+
+        if result.contains("blue") {
+            blue_result = result.split(" ").collect::<Vec<&str>>()[0];
+        }
+    }
+
+    return Game {
+        id: game_id.parse::<u32>().unwrap(),
+        red: red_result.parse::<u16>().unwrap(),
+        green: green_result.parse::<u16>().unwrap(),
+        blue: blue_result.parse::<u16>().unwrap()
+    }
+}
